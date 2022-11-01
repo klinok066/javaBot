d27a77ebfb3d115304a5716bf77fd68e1242d65b
Artem.I <klinok066@gmail.com>
2022-11-01 16:30:57 +0500

    Рефакторинг нерабочего кода: нерабочий SQL запрос

--- src/main/java/org/matmech/db/bll/GroupsDBSource.java
+++ src/main/java/org/matmech/db/bll/GroupsDBSource.java
@@ -63,7 +63,7 @@ public class GroupsDBSource {
             params.add(createParams("int", Integer.toString(groups.getDictonaryId())));
             params.add(createParams("string", groups.getTitle()));
 
-            String getGroupIdSQL = "select id from groups where dictonary_id=? and where group_title=?";
+            String getGroupIdSQL = "select id from groups where dictonary_id=? and title=?";
             ArrayList<HashMap<String, String>> response = dbConnection.executeQueryWithParams(getGroupIdSQL, params);
 
             for (HashMap<String, String> item : response)
