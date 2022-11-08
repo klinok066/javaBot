e9078f92649ef53d0c9db05766f4decfc9538e6a
iffomko <ifomko2003@mail.ru>
2022-11-08 12:11:29 +0500

    Сделал тестирование для DictonaryDBSource

--- src/test/java/TestCollector.java
+++ src/test/java/TestCollector.java
@@ -2,9 +2,10 @@ import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import TestCommands.TestRequestHandler;
+import testServiceClasses.TestDictonaryDBSource;
 import testServiceClasses.TestGroupsDBSource;
 import testServiceClasses.TestUsersDBSource;
 
-@Suite.SuiteClasses({ TestRequestHandler.class, TestGroupsDBSource.class, TestUsersDBSource.class })
+@Suite.SuiteClasses({ TestRequestHandler.class, TestGroupsDBSource.class, TestUsersDBSource.class, TestDictonaryDBSource.class })
 @RunWith(Suite.class)
 public class TestCollector {}
