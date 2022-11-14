dba32b710da5c5a83e7134ad4192b7f1029067ac
iffomko <ifomko2003@mail.ru>
2022-11-15 00:19:21 +0500

    Создал новый класс RequestsLogic, который отвечает за бизнес логику класса RequestHandler

--- src/main/java/org/matmech/requestHandler/RequestHandler.java
+++ src/main/java/org/matmech/requestHandler/RequestHandler.java
@@ -4,6 +4,7 @@ import org.matmech.cache.Cache;
 import org.matmech.dataSaver.DataSaver;
 import org.matmech.db.DBHandler;
 import org.matmech.paramsHandler.ParamsHandler;
+import org.matmech.requestsLogic.RequestsLogic;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,90 +16,35 @@ public class RequestHandler {
     private final DBHandler db;
     private final Cache cache;
     private final ParamsHandler paramsHandler;
-
-    private String toHelp() {
-        return "Bot's commands:\n" +
-                "/start - start the bot\n\n" +
-                "Groups:\n" +
-                "/group_list - list of all groups\n" +
-                "/group_create <name> - create a group\n\n" +
-                "Words:\n" +
-                "/word_list - list of all paginated words\n" +
-                "/word_add <word> <translation> <group> - adding a word. You can specify the translation yourself, but if you write _translate, then the translation will be automatic through the Yandex API, the group parameter is optional, all the rest are required\n" +
-                "/word_to <word> <group> - redefining the group of an existing word\n\n" +
-                "Testing:\n" +
-                "/test_all <mode> - start testing for all words from the database\n" +
-                "/test <count> <mode> - start testing on some part of the words from the entire database\n" +
-                "/test_in_group_all <group> <mode> - start testing for all words from the group\n" +
-                "/test_in_group <group> <count> <mode> - start testing on a certain number of words from the group\n\n" +
-                "Stopped testing:\n" +
-                "/stop_test - end testing (for /test_all and for /test_in_group_all)";
-    }
+    private final RequestsLogic requestsLogic;
 
     private boolean isCmd(String command) {
         return command.charAt(0) == '/';
     }
 
-    private String toDefaultAnswer() {
-        return "Sorry, I'm don't understand you...";
-    }
-
-
-    private String toStart(DataSaver data) {
-        return db.usersInsert(data.getFirstname(), data.getSurname(), data.getTag());
-    }
-
-    private String functionInProgress(){
-        return "So far, work is underway on this function, but in the near future it will be revived";
-    }
-
-    private String wordAdd(List<String> params, DataSaver data) {
-        return db.wordAdd(params.get(0), params.get(1), params.get(2), data.getTag());
-    }
-
-    private String translateWord(List<String> params) {
-        return db.translateWord(params.get(0));
-    }
-
-    private String edit(List<String> params) {
-        return db.edit(params.get(0), params.get(1), params.get(2));
-    }
-
-    private String getGroup(List<String> params) {
-        return db.getGroup(params.get(0));
-    }
-
-    private String deleteWord(List<String> params) {
-        return db.deleteWord(params.get(0));
-    }
-
-    private String authentication(DataSaver info) {
-        return db.userIsExist(info);
-    }
-
     private String useCommand(String command, DataSaver info, List<String> params) { // ответ на команды
         return switch (command) {
-            case "help" -> toHelp();
-            case "start" -> toStart(info);
-            case "word_add" -> wordAdd(params, info);
-            case "translate" -> translateWord(params);
-            case "remove" -> deleteWord(params);
-            case "edit" -> edit(params);
-            case "get_group" -> getGroup(params);
+            case "help" -> requestsLogic.toHelp();
+            case "start" -> requestsLogic.toStart(info);
+            case "word_add" -> requestsLogic.wordAdd(params, info);
+            case "translate" -> requestsLogic.translateWord(params);
+            case "remove" -> requestsLogic.deleteWord(params);
+            case "edit" -> requestsLogic.edit(params);
+            case "get_group" -> requestsLogic.getGroup(params);
             case    "group_list",
                     "group_create",
                     "word_list",
                     "test",
-                    "stop_test" -> functionInProgress();
+                    "stop_test" -> requestsLogic.functionInProgress();
 
-            default -> toDefaultAnswer();
+            default -> requestsLogic.toDefaultAnswer();
         };
     }
 
     private String toAnswer(String messageString, DataSaver info) { // просто ответ на обычные сообщения
         return switch (messageString.toLowerCase()) {
             case "hello" -> "Hello, " + info.getFirstname();
-            default -> toDefaultAnswer();
+            default -> requestsLogic.toDefaultAnswer();
         };
     }
 
@@ -106,12 +52,13 @@ public class RequestHandler {
         this.db = db;
         this.cache = cache;
         this.paramsHandler = new ParamsHandler(cache);
+        this.requestsLogic = new RequestsLogic(db);
     }
 
     public String processCmd(String messageString, DataSaver info) {
-        String authentication = authentication(info);
+        String authentication = requestsLogic.authentication(info);
 
-        if (authentication(info) != null)
+        if (requestsLogic.authentication(info) != null)
             return authentication;
 
         if (cache.isBusy(info.getChatId()))
---
+++ src/main/java/org/matmech/requestsLogic/RequestsLogic.java
@@ -0,0 +1,69 @@
+package org.matmech.requestsLogic;
+
+import org.matmech.dataSaver.DataSaver;
+import org.matmech.db.DBHandler;
+
+import java.util.List;
+
+public class RequestsLogic {
+    private final DBHandler db;
+
+    public RequestsLogic(DBHandler db) {
+        this.db = db;
+    }
+
+    public String toHelp() {
+        return "Bot's commands:\n" +
+                "/start - start the bot\n\n" +
+                "Groups:\n" +
+                "/group_list - list of all groups\n" +
+                "/group_create <name> - create a group\n\n" +
+                "Words:\n" +
+                "/word_list - list of all paginated words\n" +
+                "/word_add <word> <translation> <group> - adding a word. You can specify the translation yourself, but if you write _translate, then the translation will be automatic through the Yandex API, the group parameter is optional, all the rest are required\n" +
+                "/word_to <word> <group> - redefining the group of an existing word\n\n" +
+                "Testing:\n" +
+                "/test_all <mode> - start testing for all words from the database\n" +
+                "/test <count> <mode> - start testing on some part of the words from the entire database\n" +
+                "/test_in_group_all <group> <mode> - start testing for all words from the group\n" +
+                "/test_in_group <group> <count> <mode> - start testing on a certain number of words from the group\n\n" +
+                "Stopped testing:\n" +
+                "/stop_test - end testing (for /test_all and for /test_in_group_all)";
+    }
+
+    public String toDefaultAnswer() {
+        return "Sorry, I'm don't understand you...";
+    }
+
+    public String toStart(DataSaver data) {
+        return db.usersInsert(data.getFirstname(), data.getSurname(), data.getTag());
+    }
+
+    public String functionInProgress(){
+        return "So far, work is underway on this function, but in the near future it will be revived";
+    }
+
+    public String wordAdd(List<String> params, DataSaver data) {
+        return db.wordAdd(params.get(0), params.get(1), params.get(2), data.getTag());
+    }
+
+    public String translateWord(List<String> params) {
+        return db.translateWord(params.get(0));
+    }
+
+    public String edit(List<String> params) {
+        return db.edit(params.get(0), params.get(1), params.get(2));
+    }
+
+    public String getGroup(List<String> params) {
+        return db.getGroup(params.get(0));
+    }
+
+    public String deleteWord(List<String> params) {
+        return db.deleteWord(params.get(0));
+    }
+
+    public String authentication(DataSaver info) {
+        return db.userIsExist(info);
+    }
+}
