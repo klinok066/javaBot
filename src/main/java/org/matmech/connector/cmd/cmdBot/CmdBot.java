e501b260784e66fc5287a7b3dac8a0f79b046c86
iffomko <ifomko2003@mail.ru>
2022-11-21 21:41:48 +0500

    исправил авторизацию пользователя, убрал добавление информации о пользователе из полей в Map, который теперь возвращает метод initUser

--- src/main/java/org/matmech/connector/cmd/cmdBot/CmdBot.java
+++ src/main/java/org/matmech/connector/cmd/cmdBot/CmdBot.java
@@ -1,54 +1,56 @@
-package org.matmech.connector.cmd.cmdBot;
+package org.matmech.connector.cmdBot;
 
 import org.matmech.connector.Connector;
-import org.matmech.connector.cmd.cmdLogic.CmdLogic;
-import org.matmech.context.contextManager.ContextManager;
+import org.matmech.connector.cmdLogic.CmdLogic;
+import org.matmech.requestHandler.RequestHandler;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
  * Класс-интерфейс, который реализует консольную обертку над ботом
  */
 public class CmdBot implements Connector {
-    private String firstName;
-    private String lastName;
-    private String tag;
-    private int chatId;
     private final Scanner input;
-    private final ContextManager contextManager;
+    private final RequestHandler requestHandler;
 
     private CmdLogic bot;
 
     /**
      * Инициализирует пользователя, который пользуется сейчас консольным ботом
      */
-    private void initUser() {
+    private Map<String, String> initUser() {
+        Map<String, String> userData = new HashMap<String, String>();
+
         System.out.println("Добро пожаловать! Авторизуйте свой компьютер!");
         System.out.print("Введите свое имя:");
-        this.firstName = input.nextLine();
+        userData.put("firstname", input.nextLine());
+
         System.out.print("Введите свою фамилию:");
-        this.lastName = input.nextLine();
+        userData.put("lastname", input.nextLine());
+
         System.out.print("Введите/придумайте себе тег:");
-        this.tag = input.nextLine();
-        System.out.print("Введите/придумайте себе id чата:");
-        this.chatId = input.nextInt();
+        userData.put("tag", input.nextLine());
 
         System.out.println("Вы были успешно авторизированы! Для того, чтобы начать работать с ботом, напишите /start");
+
+        return userData;
     }
 
-    public CmdBot(ContextManager contextManager) {
-        this.contextManager = contextManager;
+    public CmdBot(RequestHandler requestHandler) {
+        this.requestHandler = requestHandler;
         this.input = new Scanner(System.in);
 
-        initUser();
+        Map<String, String> userData = initUser();
 
-        bot = new CmdLogic(firstName, lastName, tag, chatId, contextManager);
+        bot = new CmdLogic(userData.get("firstname"), userData.get("lastname"), userData.get("tag"), requestHandler);
     }
 
     /**
-     * Запускает бота
+     * Этот метод запускает консольного бота
      */
     public void start() {
-        bot.handle();
+        bot.responseForCDM();
     }
 }
