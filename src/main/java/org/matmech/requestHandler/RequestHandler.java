c80feaffb34aa8f5f67fdd053262b76885f8aba8
iffomko <ifomko2003@mail.ru>
2022-11-21 22:05:08 +0500

    Убрал ненужные комментарии

--- src/main/java/org/matmech/requestHandler/RequestHandler.java
+++ src/main/java/org/matmech/requestHandler/RequestHandler.java
@@ -65,14 +65,6 @@ public class RequestHandler {
         return db.deleteWord(params.get(0));
     }
 
-//    private String setMode(ArrayList<String> params, DataSaver data) {
-//        return db.setMode(params.get(0), data.getTag());
-//    }
-//
-//    private String getMode(DataSaver data) {
-//        return db.getMode(data.getTag());
-//    }
-
     private String useCommand(String command, UserData info, List<String> params) { // ответ на команды
         return switch (command) {
             case "help" -> toHelp();
@@ -82,8 +74,6 @@ public class RequestHandler {
             case "remove" -> deleteWord(params);
             case "edit" -> edit(params);
             case "get_group" -> getGroup(params);
-//            case "set_mode" -> setMode(params, info);
-//            case "get_mode" -> getMode(info);
             case    "group_list",
                     "group_create",
                     "word_list",
